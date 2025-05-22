b3161d5fab1dcc77065173aa2c86eabf958a2974
Balamurugan1962 <145089896+Balamurugan1962@users.noreply.github.com>
2025-05-23 00:01:14 +0530

    Update Database.py

--- utils/Database.py
+++ utils/Database.py
@@ -7,32 +7,46 @@ class MongoDB:
     def __init__(self, MongoDBAPI):
         self.client = MongoClient(MongoDBAPI)
         self.db = self.client["ResturentManagement"]
-    
+        
     def insertData(self,collection,id,data):
         collection = self.db[collection]
+        
         if collection is None:
             return False
         
-        result = collection.insert_one({id:data})
+        print("Hello")
+        print(id,data)
+        
+        result = collection.insert_one({"id" : id, "data" :data})
+        
         print(result)
+
         return result.acknowledged
+
+    def qurryData(self,collection,qurry):
+        collection = self.db[collection]
+        if collection is None:
+            return None
+        
+        result = collection.find(qurry)
+        return result
     
     
 
 #test runner code
 if __name__ == "__main__":
-    # Example usage
-    # MongoDBAPI = "mongodb+srv://<username>:<password>@cluster.mongodb.net/test?retryWrites=true&w=majority"
-    # db = MongoDB(MongoDBAPI)
-    # db.insertData("Auth", "U001", {"name": "Bala","password": "1234","role": "admin"})
+
     load_dotenv("/Users/balamurugan/Documents/GitHub/sap-backend/.env")
     MongoDBAPI = os.getenv("MongoDBAPI")
+
     if MongoDBAPI is None:
         raise ValueError("MongoDB API key not found in environment variables.")
 
     db = MongoDB(MongoDBAPI)
-    db.insertData("Auth", "U001", {"name": "Bala","password": "1234","role": "admin"})
 
+    db.insertData("Auth", "U0010", {"name": "Ram","password": "4321","role": "employee"})
 
+    result = db.qurryData("Auth",{"id" : "U0010"})
 
-
+    for i in result:
+        print(i)
